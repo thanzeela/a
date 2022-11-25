200109b2749dafe49e8b75ff84370574f5f73317
Thanzeela K A <thanzeela.ka@experionglobal.local>
2022-11-25 10:20:08 +0530

    six

--- app.js
+++ app.js
@@ -3,15 +3,32 @@ const parser =require('body-parser');
 const customerRoute = require('./routes/customer')
 const loginRoutes = require('./routes/login');
 const path = require('path');
+const cookieSession = require('cookie-session');
 const {engine} = require('express-handlebars');
+const authMiddleware = require('./middlewares/authenticationMiddleware');
+
 const app = express();
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 
 app.use("/", parser.urlencoded({extended: true}));
 app.use("/static", express.static(path.join(__dirname, 'static')));
+app.use(cookieSession({
+    name: 'session',
+    httpOnly: true,
+    keys: ["asdghjhgsdahjsgdhjasd"],
+    maxAge: 24 * 60 * 60 * 1000
+}));
+
+app.use(authMiddleware);
 app.use(customerRoute);
 app.use(loginRoutes);
+
+
+function handler(req, res){
+    res.send("Hello World")
+}
+app.get('/test', handler)
 
 
 app.listen(5000);
---
+++ middlewares/authenticationMiddleware.js
@@ -0,0 +1,32 @@
+const user = require('../models/customer');
+
+module.exports = async (req, res, next) => {
+    req.identity = {
+        isAuthenticated: false,
+        user: null
+    }
+
+    if(req.url == "/login" || req.url == "/create"){
+        return next();
+    }
+
+    let userId = req.session.userId;
+    if(!userId || userId == null){
+        return res.redirect("/login");
+    }
+
+    let userFromDb = await user.findByPk(userId);
+    if(userFromDb == null){
+        return res.redirect("/login");
+    }
+
+    req.identity.isAuthenticated = true;
+    req.identity.customer = {
+        id: userFromDb.dataValues.id,
+        firstName: userFromDb.dataValues.firstName,
+        lastName: userFromDb.dataValues.lastName,
+        email: userFromDb.dataValues.email,
+        role: 'user'
+    }
+    next();
+}
